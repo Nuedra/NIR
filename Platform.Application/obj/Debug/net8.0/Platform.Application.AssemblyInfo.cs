cc7cceef12e98c42659cd2d3252f2d7baef973cc
vladislav <vladdd019@gmail.com>
2026-03-29 00:29:19 +0300

    add README

--- Platform.Application/obj/Debug/net8.0/Platform.Application.AssemblyInfo.cs
+++ Platform.Application/obj/Debug/net8.0/Platform.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Platform.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fec909d2fdcdf7073b2f6ed8dea67f2021de847f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Platform.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Platform.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ README.md
@@ -0,0 +1,33 @@
+# NIR Platform
+
+## Требования
+
+- .NET SDK 8.0 (проверка: `dotnet --version`)
+- macOS/Linux/Windows с установленным `dotnet`
+
+## Структура решения
+
+В решении `Platform.sln` есть несколько проектов, точка запуска веб-приложения:
+
+- `Platform.Application` (Blazor Server, основной исполняемый проект)
+
+## Сборка проекта
+
+Из корня репозитория выполните:
+
+```bash
+dotnet restore "Platform.sln"
+dotnet build "Platform.sln"
+```
+
+## Запуск проекта
+
+Запуск из корня репозитория:
+
+```bash
+dotnet run --project "Platform.Application/Platform.Application.csproj"
+```
+
+После старта приложение доступно по адресу:
+
+- `http://localhost:5284`
